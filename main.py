f122d205b3df9c801a95134888387eed582c857c
FilinMen <fiilinkadabra@tutamail.com>
2024-10-15 14:56:16 +0300

    work in program, but in acmp.ru don't work

--- main.py
+++ main.py
@@ -6,30 +6,23 @@ b = int(data[1])
 c = int(data[2])
 d = int(data[3])
 for x1 in range(-100,101):
-        if a == 0 :
-            y = str(a)
-        output_data = open("output.txt","w")
-        output_data.write(y)
-else:
-    a = int(data[0])    
-    for x1 in range(-100,101):
-        if a * (x1 ** 3) + b * (x1 ** 2) + c * x1 + d == 0:
-            p = x1 + 1
-            break
-        else:
-            x1 = " "
+    if a * (x1 ** 3) + b * (x1 ** 2) + c * x1 + d == 0:
+        p = x1 + 1
+        break
+    else:
+        x1 = " "
 for x2 in range(p,101):
     if  a * (x2 ** 3) + b * (x2 ** 2) + c * x2 + d == 0:
         o = x2 + 1
         break
     else:
-     x2 = " "
+        x2 = " "
 for x3 in range(o,101):
     if a * (x3 ** 3) + b * (x3 ** 2) + c * x3 + d == 0:
         i = x3 + 1
         break
     else:
-         x3 = " "        
+        x3 = " "        
 v = str(x1) + " " + str(x2) + " " + str(x3)
 output_data = open("output.txt","w")
 output_data.write(v)
